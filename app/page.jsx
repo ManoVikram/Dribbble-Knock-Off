8281495b6976ed763d94f985bf847a9ea036ca49
Mano Vikram <manovik18@gmail.com>
2025-03-05 02:55:13 +0530

    Added API call to get all projects from DB

--- app/page.jsx
+++ app/page.jsx
@@ -1,10 +1,38 @@
-import Image from "next/image";
+import { auth } from "@/auth";
+import ProjectCard from "@components/ProjectCard";
 
-export default function Home() {
+export default async function Home() {
+  const session = await auth()
+
+  const response = await fetch("http://localhost:8080/api/allprojects", {
+    method: "GET",
+    headers: {
+      "Content-Type": "application/json",
+      "Authorization": `Bearer ${session.sessionToken}`
+    },
+  })
+  const projects = await response.json()
+
+  if (projects.length === 0) {
+    return (
+      <section className="flexStart flex-col paddings">
+        Categories
+
+        <p className="no-result-text text-center">No projects found, go create one first.</p>
+      </section>
+    )
+  }
+
   return (
     <section className="flexStart flex-col paddings mb-16">
       Categories
-      Posts
+      
+      <section className="projects-grid">
+        {projects.map((project) => (
+          <ProjectCard key={project.id} project={project} />
+        ))}
+      </section>
+
       LoadMore
     </section>
   );
